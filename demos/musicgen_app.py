a1a1ef4f3fc447e6bb8206ad82c49768528d66a9
Sabian Hibbs <Sabian.hibbs@gmail.com>
2024-02-29 08:37:30 +0000

    Update musicgen_app.py

--- demos/musicgen_app.py
+++ demos/musicgen_app.py
@@ -1,12 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-# Updated to account for UI changes from https://github.com/rkfg/audiocraft/blob/long/app.py
-# also released under the MIT license.
-
 import argparse
 from concurrent.futures import ProcessPoolExecutor
 import logging
@@ -91,7 +82,7 @@ def make_waveform(*args, **kwargs):
         return out
 
 
-def load_model(version='facebook/musicgen-melody'):
+def load_model(version='facebook/musicgen-stereo-melody'):
     global MODEL
     print("Loading model", version)
     if MODEL is None or MODEL.name != version:
@@ -114,7 +105,7 @@ def _do_predictions(texts, melodies, duration, progress=False, gradio_progress=N
     print("new batch", len(texts), texts, [None if m is None else (m[0], m[1].shape) for m in melodies])
     be = time.time()
     processed_melodies = []
-    target_sr = 32000
+    target_sr = 44100
     target_ac = 1
     for melody in melodies:
         if melody is None:
@@ -243,21 +234,13 @@ def toggle_diffusion(choice):
 
 def ui_full(launch_kwargs):
     with gr.Blocks() as interface:
-        gr.Markdown(
-            """
-            # MusicGen
-            This is your private demo for [MusicGen](https://github.com/facebookresearch/audiocraft),
-            a simple and controllable model for music generation
-            presented at: ["Simple and Controllable Music Generation"](https://huggingface.co/papers/2306.05284)
-            """
-        )
         with gr.Row():
             with gr.Column():
                 with gr.Row():
-                    text = gr.Text(label="Input Text", interactive=True)
+                    text = gr.Text(label="Music Generation Prompt", interactive=True)
                     with gr.Column():
                         radio = gr.Radio(["file", "mic"], value="file",
-                                         label="Condition on a melody (optional) File or Mic")
+                                         label="Add Fine Tuned Prompt (optional) File or Mic")
                         melody = gr.Audio(sources=["upload"], type="numpy", label="File",
                                           interactive=True, elem_id="melody-input")
                 with gr.Row():
@@ -265,23 +248,17 @@ def ui_full(launch_kwargs):
                     # Adapted from https://github.com/rkfg/audiocraft/blob/long/app.py, MIT license.
                     _ = gr.Button("Interrupt").click(fn=interrupt, queue=False)
                 with gr.Row():
-                    model = gr.Radio(["facebook/musicgen-melody", "facebook/musicgen-medium", "facebook/musicgen-small",
-                                      "facebook/musicgen-large", "facebook/musicgen-melody-large",
-                                      "facebook/musicgen-stereo-small", "facebook/musicgen-stereo-medium",
-                                      "facebook/musicgen-stereo-melody", "facebook/musicgen-stereo-large",
-                                      "facebook/musicgen-stereo-melody-large"],
-                                     label="Model", value="facebook/musicgen-stereo-melody", interactive=True)
-                    model_path = gr.Text(label="Model Path (custom models)")
+                    model = gr.Radio([""], label="Model", value="facebook/musicgen-stereo-melody", interactive=False, visible=False)
+                    model_path = gr.Text(label="Model Path (custom models)", visible=False)
                 with gr.Row():
-                    decoder = gr.Radio(["Default", "MultiBand_Diffusion"],
-                                       label="Decoder", value="Default", interactive=True)
+                    decoder = gr.Radio(["Default", "MultiBand_Diffusion"], label="Decoder", value="Default", interactive=False, visible=False)
                 with gr.Row():
                     duration = gr.Slider(minimum=1, maximum=120, value=10, label="Duration", interactive=True)
                 with gr.Row():
-                    topk = gr.Number(label="Top-k", value=250, interactive=True)
-                    topp = gr.Number(label="Top-p", value=0, interactive=True)
-                    temperature = gr.Number(label="Temperature", value=1.0, interactive=True)
-                    cfg_coef = gr.Number(label="Classifier Free Guidance", value=3.0, interactive=True)
+                    topk = gr.Number(label="Top-k", value=250, interactive=True, visible=False)
+                    topp = gr.Number(label="Top-p", value=0, interactive=True, visible=False)
+                    temperature = gr.Number(label="Temperature", value=1.0, interactive=True, visible=False)
+                    cfg_coef = gr.Number(label="Classifier Free Guidance", value=3.0, interactive=True, visible=False)
             with gr.Column():
                 output = gr.Video(label="Generated Music")
                 audio_output = gr.Audio(label="Generated Music (wav)", type='filepath')
@@ -293,114 +270,12 @@ def ui_full(launch_kwargs):
                                                outputs=[output, audio_output, diffusion_output, audio_diffusion])
         radio.change(toggle_audio_src, radio, [melody], queue=False, show_progress=False)
 
-        gr.Examples(
-            fn=predict_full,
-            examples=[
-                [
-                    "An 80s driving pop song with heavy drums and synth pads in the background",
-                    "./assets/bach.mp3",
-                    "facebook/musicgen-stereo-melody",
-                    "Default"
-                ],
-                [
-                    "A cheerful country song with acoustic guitars",
-                    "./assets/bolero_ravel.mp3",
-                    "facebook/musicgen-stereo-melody",
-                    "Default"
-                ],
-                [
-                    "90s rock song with electric guitar and heavy drums",
-                    None,
-                    "facebook/musicgen-stereo-medium",
-                    "Default"
-                ],
-                [
-                    "a light and cheerly EDM track, with syncopated drums, aery pads, and strong emotions",
-                    "./assets/bach.mp3",
-                    "facebook/musicgen-stereo-melody",
-                    "Default"
-                ],
-                [
-                    "lofi slow bpm electro chill with organic samples",
-                    None,
-                    "facebook/musicgen-stereo-medium",
-                    "Default"
-                ],
-                [
-                    "Punk rock with loud drum and power guitar",
-                    None,
-                    "facebook/musicgen-stereo-medium",
-                    "MultiBand_Diffusion"
-                ],
-            ],
-            inputs=[text, melody, model, decoder],
-            outputs=[output]
-        )
-        gr.Markdown(
-            """
-            ### More details
-
-            The model will generate a short music extract based on the description you provided.
-            The model can generate up to 30 seconds of audio in one pass.
-
-            The model was trained with description from a stock music catalog, descriptions that will work best
-            should include some level of details on the instruments present, along with some intended use case
-            (e.g. adding "perfect for a commercial" can somehow help).
-
-            Using one of the `melody` model (e.g. `musicgen-melody-*`), you can optionally provide a reference audio
-            from which a broad melody will be extracted.
-            The model will then try to follow both the description and melody provided.
-            For best results, the melody should be 30 seconds long (I know, the samples we provide are not...)
-
-            It is now possible to extend the generation by feeding back the end of the previous chunk of audio.
-            This can take a long time, and the model might lose consistency. The model might also
-            decide at arbitrary positions that the song ends.
-
-            **WARNING:** Choosing long durations will take a long time to generate (2min might take ~10min).
-            An overlap of 12 seconds is kept with the previously generated chunk, and 18 "new" seconds
-            are generated each time.
-
-            We present 10 model variations:
-            1. facebook/musicgen-melody -- a music generation model capable of generating music condition
-                on text and melody inputs. **Note**, you can also use text only.
-            2. facebook/musicgen-small -- a 300M transformer decoder conditioned on text only.
-            3. facebook/musicgen-medium -- a 1.5B transformer decoder conditioned on text only.
-            4. facebook/musicgen-large -- a 3.3B transformer decoder conditioned on text only.
-            5. facebook/musicgen-melody-large -- a 3.3B transformer decoder conditioned on and melody.
-            6. facebook/musicgen-stereo-*: same as the previous models but fine tuned to output stereo audio.
-
-            We also present two way of decoding the audio tokens
-            1. Use the default GAN based compression model. It can suffer from artifacts especially
-                for crashes, snares etc.
-            2. Use [MultiBand Diffusion](https://arxiv.org/abs/2308.02560). Should improve the audio quality,
-                at an extra computational cost. When this is selected, we provide both the GAN based decoded
-                audio, and the one obtained with MBD.
-
-            See [github.com/facebookresearch/audiocraft](https://github.com/facebookresearch/audiocraft/blob/main/docs/MUSICGEN.md)
-            for more details.
-            """
-        )
-
+        
         interface.queue().launch(**launch_kwargs)
 
 
 def ui_batched(launch_kwargs):
     with gr.Blocks() as demo:
-        gr.Markdown(
-            """
-            # MusicGen
-
-            This is the demo for [MusicGen](https://github.com/facebookresearch/audiocraft/blob/main/docs/MUSICGEN.md),
-            a simple and controllable model for music generation
-            presented at: ["Simple and Controllable Music Generation"](https://huggingface.co/papers/2306.05284).
-            <br/>
-            <a href="https://huggingface.co/spaces/facebook/MusicGen?duplicate=true"
-                style="display: inline-block;margin-top: .5em;margin-right: .25em;" target="_blank">
-            <img style="margin-bottom: 0em;display: inline;margin-top: -.25em;"
-                src="https://bit.ly/3gLdBN6" alt="Duplicate Space"></a>
-            for longer sequences, more control and no queue.</p>
-            """
-        )
         with gr.Row():
             with gr.Column():
                 with gr.Row():
@@ -418,59 +293,7 @@ def ui_batched(launch_kwargs):
         submit.click(predict_batched, inputs=[text, melody],
                      outputs=[output, audio_output], batch=True, max_batch_size=MAX_BATCH_SIZE)
         radio.change(toggle_audio_src, radio, [melody], queue=False, show_progress=False)
-        gr.Examples(
-            fn=predict_batched,
-            examples=[
-                [
-                    "An 80s driving pop song with heavy drums and synth pads in the background",
-                    "./assets/bach.mp3",
-                ],
-                [
-                    "A cheerful country song with acoustic guitars",
-                    "./assets/bolero_ravel.mp3",
-                ],
-                [
-                    "90s rock song with electric guitar and heavy drums",
-                    None,
-                ],
-                [
-                    "a light and cheerly EDM track, with syncopated drums, aery pads, and strong emotions bpm: 130",
-                    "./assets/bach.mp3",
-                ],
-                [
-                    "lofi slow bpm electro chill with organic samples",
-                    None,
-                ],
-            ],
-            inputs=[text, melody],
-            outputs=[output]
-        )
-        gr.Markdown("""
-        ### More details
-
-        The model will generate 15 seconds of audio based on the description you provided.
-        The model was trained with description from a stock music catalog, descriptions that will work best
-        should include some level of details on the instruments present, along with some intended use case
-        (e.g. adding "perfect for a commercial" can somehow help).
-
-        You can optionally provide a reference audio from which a broad melody will be extracted.
-        The model will then try to follow both the description and melody provided.
-        For best results, the melody should be 30 seconds long (I know, the samples we provide are not...)
-
-        You can access more control (longer generation, more models etc.) by clicking
-        the <a href="https://huggingface.co/spaces/facebook/MusicGen?duplicate=true"
-                style="display: inline-block;margin-top: .5em;margin-right: .25em;" target="_blank">
-            <img style="margin-bottom: 0em;display: inline;margin-top: -.25em;"
-                src="https://bit.ly/3gLdBN6" alt="Duplicate Space"></a>
-        (you will then need a paid GPU from HuggingFace).
-        If you have a GPU, you can run the gradio demo locally (click the link to our repo below for more info).
-        Finally, you can get a GPU for free from Google
-        and run the demo in [a Google Colab.](https://ai.honu.io/red/musicgen-colab).
-
-        See [github.com/facebookresearch/audiocraft](https://github.com/facebookresearch/audiocraft/blob/main/docs/MUSICGEN.md)
-        for more details. All samples are generated with the `stereo-melody` model.
-        """)
-
+        
         demo.queue(max_size=8 * 4).launch(**launch_kwargs)
 
 
@@ -514,7 +337,6 @@ if __name__ == "__main__":
         launch_kwargs['inbrowser'] = args.inbrowser
     if args.share:
         launch_kwargs['share'] = args.share
-
     logging.basicConfig(level=logging.INFO, stream=sys.stderr)
 
     # Show the interface
